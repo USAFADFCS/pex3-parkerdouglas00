5f92c11bbaf652e6869ca00066356ead840285f1
Parker Douglas <c26parker.douglas@afacademy.af.edu>
2026-04-23 14:34:35 -0600

    pqAccess implemented

--- pagequeue.c
+++ pagequeue.c
@@ -42,7 +42,69 @@ long pqAccess(PageQueue *pq, unsigned long pageNum) {
     //   - Allocate a new node for pageNum and insert it at the tail.
     //   - If size now exceeds maxSize, evict the head node (free it).
     //   - Return -1.
-        return -1;
+    PqNode* currentNode = pq->tail;
+    long d = 0;
+
+    while (currentNode != NULL) {
+        // HIT
+        if (currentNode->pageNum == pageNum) {
+            // if the hit node isn't the tail already
+            if (currentNode != pq->tail) {
+                // if currentNode is not head
+            if (currentNode->prev != NULL) {
+                currentNode->prev->next = currentNode->next;
+            }
+            // if currentNode is head
+            if (currentNode == pq->head) {
+                pq->head = currentNode->next;
+            }
+            currentNode->next->prev = currentNode->prev;
+            currentNode->prev = pq->tail;
+            currentNode->next = NULL;
+            pq->tail->next = currentNode;
+            pq->tail = currentNode;
+            }
+            return d;
+        }
+        d++;
+        currentNode = currentNode->prev;
+    }
+
+    // MISS
+    PqNode* newNode = (PqNode*) malloc(sizeof(PqNode));
+    newNode->pageNum = pageNum;
+    newNode->next = NULL;
+    newNode->prev = pq->tail;
+
+    // queue is not empty
+    if (pq->tail != NULL) {
+        pq->tail->next = newNode;
+    }
+    // queue is empty
+    else {
+        pq->head = newNode;
+    }
+
+    pq->tail = newNode;
+    pq->size++;
+
+    //size now exceeds maxSize
+    if (pq->size > pq->maxSize) {
+        PqNode* nodeToEvict = pq->head;
+        pq->head = pq->head->next;
+        // if evicted node was not only node
+        if (pq->head != NULL) {
+            pq->head->prev = NULL;
+        }
+        // if evicted node was only node
+        else {
+            pq->tail = NULL;
+        }
+        free(nodeToEvict);
+        pq->size--;
+    }
+    
+    return -1;
 }
 
 /**
